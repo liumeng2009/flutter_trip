48a2c044112191f98f6190cb1a31391e658d04a2
liumeng <378338627@qq.com>
2019-04-22 17:03:12 +0800

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'https://hbimg.huabanimg.com/2de4d342c1767616c0307733f231978f16159be511a14-jXX4jo_fw658',
+    'https://hbimg.huabanimg.com/043f46e618db6167bab76d66526b4d1430eb970269e0a-3AooUa_fw658',
+    'https://hbimg.huabanimg.com/acb67d0867fc191ac3a0e3a469151ba1948c31dc26788-J6L697_fw658',
+  ];
+  _onScroll(offset) {
+    print(offset);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                          );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('哈哈'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: 1,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ), 
+    );
+  }
+}
